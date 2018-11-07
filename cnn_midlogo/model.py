35686c2a8b49121f5b2b8da2fa1840118f2496fa
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-11-07 16:28:23 +0100

    model: inception v3 fine tuning
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

---
+++ cnn_midlogo/model.py
@@ -0,0 +1,38 @@
+
+""" Keras Model."""
+
+from keras.applications import inception_v3
+from keras.models import Model
+from keras.layers import Dense, Dropout, GlobalAveragePooling2D
+
+
+def _set_readonly(model):
+    """Make a model weights readonly."""
+    for layer in model.layers:
+        layer.trainable = False
+    return model
+
+
+def _classification(model, output_shape):
+    """Add classification layers."""
+    x = model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.4)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.4)(x)
+    x = Dense(output_shape, activation='softmax')(x)
+
+    return Model(inputs=model.input, outputs=x)
+
+
+def get_model(output_shape):
+    """Get the model."""
+    # load the Inception_V3 model
+    model = inception_v3.InceptionV3(weights='imagenet', include_top=False)
+    # make them readonly
+    model = _set_readonly(model)
+    # add new classification layers
+    model = _classification(model, output_shape)
+
+    return model
