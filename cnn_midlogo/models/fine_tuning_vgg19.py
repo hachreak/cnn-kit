fcf55b856a11447e7461f5cb972b04fc26164540
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-11-09 16:25:12 +0100

    models: fine tuning vgg19
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

---
+++ cnn_midlogo/models/fine_tuning_vgg19.py
@@ -0,0 +1,22 @@
+
+""" Keras Model."""
+
+from keras.applications import vgg19
+
+from . import _set_readonly, _classification
+
+
+def get_model(input_shape, output_shape):
+    """Get the model."""
+    # load the Inception_V3 model
+    model = vgg19.VGG19(
+        weights='imagenet',
+        include_top=False,
+        input_shape=input_shape
+    )
+    # make them readonly
+    model = _set_readonly(model)
+    # add new classification layers
+    model = _classification(model, output_shape)
+
+    return model
