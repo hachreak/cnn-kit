3cdac677e14f685790b15c3c7a5621f4a8dfab23
Leonardo Rossi <leonardo.rossi@unipr.it>
2018-12-11 18:20:01 +0100

    cli: build dataset from csv
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

--- cnn_kit/cli/main.py
+++ cnn_kit/cli/main.py
@@ -4,7 +4,7 @@
 import click
 
 from .validators import validate_csv_file, validate_directory
-from ..datasets.csv import find_files
+from ..datasets.csv import find_files, build_dataset, create_symlinks
 
 
 @click.group()
@@ -18,16 +18,15 @@ def dataset():
 
 
 @dataset.group()
-def load():
+def build():
     pass
 
 
-@load.command()
+@build.command()
 @click.argument('csv_file', callback=validate_csv_file, type=click.File('rb'))
 @click.argument('src_dir', callback=validate_directory)
-@click.argument('dst_dir', callback=validate_directory)
+@click.argument('dst_dir')
 def csv(csv_file, src_dir, dst_dir):
     csv_file = list(csv_file)
     files = find_files(csv_file, src_dir)
-    for i in files:
-        print(i)
+    create_symlinks(build_dataset(csv_file, files, dst_dir))
--- cnn_kit/datasets/csv.py
+++ cnn_kit/datasets/csv.py
@@ -1,22 +1,66 @@
 
 """Load and save dataset from/to csv."""
 
+import errno
 import os
 
 from .. import preprocess as pr
+
+
+def build_dataset(csv_file, filepaths, dst_dir):
+    """Build dataset from csv, using images found."""
+    for name, set_, class_ in csv_file[1:]:
+        # get full file path from only the name
+        paths = filter(
+            lambda x: x.endswith("/{0}".format(name)), filepaths
+        )
+        # only if file is found
+        if len(paths) > 0:
+            fname = paths[0]
+            yield fname, os.path.join(dst_dir, set_, class_, name)
 
 
 def find_files(csv_file, src_dir):
     """Find dataset files from csv file."""
     fcsv = csv_file[1:]
     filenames = [name for name, _, _ in fcsv]
-    exts = get_files_ext(filenames)
+    exts = _get_files_ext(filenames)
     return filter(
         lambda x: os.path.basename(x) in filenames,
         pr.get_files(src_dir, types=exts)
     )
 
 
-def get_files_ext(filenames):
+def _get_files_ext(filenames):
     """Get filenames extentions."""
     return set([os.path.splitext(filename)[1] for filename in filenames])
+
+
+def create_symlinks(srcs_dsts):
+    """create file symlinks."""
+    for src, dst in srcs_dsts:
+        directory, _ = os.path.split(dst)
+        _create_dir(directory)
+        _create_symlink(src, dst, True)
+
+
+def _create_symlink(src, dst, force=True):
+    print("{0} -> {1}".format(src, dst))
+    try:
+        os.symlink(src, dst)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and force:
+            print("remove {0}".format(dst))
+            os.remove(dst)
+            _create_symlink(src, dst, force)
+
+
+def _create_dir(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
