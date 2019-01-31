71b43b1619aefa70a97274b159a83360c5372613
Leonardo Rossi <leonardo.rossi@unipr.it>
2019-01-31 12:02:59 +0100

    siamese: generalize distance computing
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

--- cnn_kit/models/siamese.py
+++ cnn_kit/models/siamese.py
@@ -9,17 +9,31 @@ from keras.models import Model
 from keras import backend as K
 
 
-def get_model(input_shape, model, output_shape):
+def get_model(input_shape, model, output_shape, fun=None):
     """Get the model."""
+    fun = fun or abs_diff
+
     left_input = Input(input_shape)
     right_input = Input(input_shape)
 
     encoded_l = model(left_input)
     encoded_r = model(right_input)
 
-    L1_layer = Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]))
+    L1_layer = Lambda(fun)
     L1_distance = L1_layer([encoded_l, encoded_r])
 
     prediction = Dense(output_shape, activation='sigmoid')(L1_distance)
 
     return Model(inputs=[left_input, right_input], outputs=prediction)
+
+
+def abs_diff(tensors):
+    """Make abs(A - B)."""
+    return K.abs(tensors[0] - tensors[1])
+
+
+def euclidean_distance(tensors):
+    """Make euclidean distance."""
+    return K.sqrt(
+        (K.square(tensors[0] - tensors[1])).sum(axis=1, keepdims=True)
+    )
